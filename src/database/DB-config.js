d3f890af6df9d11c6e7a2a5d23b598ac68c9f6e2
Deadlift3008 <deadlift3008@gmail.com>
2021-07-20 12:51:37 +0300

    fix envs

--- src/database/DB-config.js
+++ src/database/DB-config.js
@@ -2,6 +2,6 @@ module.exports = {
     host: process.env.HOST || 'localhost',
     user: process.env.USER || 'root',
     password: process.env.PASSWORD || 'password',
-    database: process.env.NODE_ENV === 'production' ? 'social_network_prod' : 'social_network_dev',
+    database: process.env.NODE_ENV === 'production' ? process.env.DATABASE : 'social_network_dev',
     port: process.env.PORT || 3306
 };
